9526e7520ce2ab924bc972ef215be4e9227fb3ae
“Santhosh” <santhoshvelr@gmail.com>
2020-05-06 12:56:50 +0530

    added view more details

---
+++ frontend/src/components/ViewDetails.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Modal } from "react-bootstrap";
+import GetAppIcon from '@material-ui/icons/GetApp';
+
+function ViewDetails(props) {
+  const [smShow, setSmShow] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setSmShow(true)}>view more</Button>
+      <Modal
+        dialogClassName="modal-100w"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+          <h2>{props.data.title}</h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <p>{props.data.dueDate}</p>
+            <h3>{props.data.subName}</h3>
+            <h4>{props.data.author}</h4>
+            <p>{props.data.description}</p>  
+            <h5>{props.data.file}</h5>
+            {props.data.url !== "" && <a href={props.data.url}><GetAppIcon className="DowIco" /></a> }
+            <p>{props.data.type}</p>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default ViewDetails;
