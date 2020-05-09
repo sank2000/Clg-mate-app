fb7d9309cd39bebd783a43a4b2216f8221d356fa
“Santhosh” <santhoshvelr@gmail.com>
2020-05-09 22:19:31 +0530

    redirect fixed in success and error msg

--- frontend/src/components/messages/SuccessMessage.jsx
+++ frontend/src/components/messages/SuccessMessage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Jumbotron, Button } from "react-bootstrap";
-import { useHistory } from 'react-router';
 
 function SuccessMessage() {
-    const history = useHistory();
     return (
         <Jumbotron fluid>
             <h1>Post added</h1>
             <p>You cannot edit/ remove your post as of now. But this feature will be available soon.</p>
             <p>
                 <Button onClick={() => {
-                    history.push("/post");
+                    window.open("/");
                 }} variant="primary">Go To Home</Button>
             </p>
         </Jumbotron>
--- frontend/src/components/messages/errors/404.jsx
+++ frontend/src/components/messages/errors/404.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Jumbotron, Button } from "react-bootstrap";
-import { useHistory } from 'react-router';
 
 function Emsg() {
-    const history = useHistory();
     return <Jumbotron fluid>
         <h1>404 Not found <span role="img" aria-label="emoji">🤔</span></h1>
         <p>oops something went wrong <span role="img" aria-label="emoji">😔</span></p>
         <p>
             <Button onClick={() => {
-                history.push("/");
+                window.open("/");
             }} variant="primary">Go To Home</Button>
         </p>
     </Jumbotron>
