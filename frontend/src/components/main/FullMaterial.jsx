67ea23f2b7dd29247d164d76a54304a5332d0518
“Santhosh” <santhoshvelr@gmail.com>
2020-05-11 21:45:25 +0530

    search button added without functionality

--- frontend/src/components/main/FullMaterial.jsx
+++ frontend/src/components/main/FullMaterial.jsx
@@ -3,13 +3,32 @@ import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import DateFormat from 'dateformat';
 import LinearProgress from '@material-ui/core/LinearProgress';
-
+import ClearOutlinedIcon from "@material-ui/icons/ClearOutlined";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import IconButton from "@material-ui/core/IconButton";
 import NavigationBar from "../navigation/AppBar";
 import MaterialCard from "../cards/MaterialCard";
 
 function App() {
     const [post, setPost] = useState([]);
     const [loading, SetLoading] = useState(true);
+    const [search, setSearch] = useState("");
+
+    function handleSearch() {
+        console.log(search);
+    }
+     
+    function handleCancel()
+    {
+        setSearch("");
+    }
+
+    function SearchText(event) {
+        setSearch(event.target.value);
+    }
 
     useEffect(() => {
         // fetch("/work").then(res => console.log(res.json()));
@@ -45,8 +64,37 @@ function App() {
     return (
         <Fragment>
             <NavigationBar />
+            <Container fluid className="fullPostHead">
+                <Row>
+                    <Col lg={9} xs={6}><h1 style={{ display: "inline" }}>Materials</h1></Col>
+                    <Col >
+                    <FormControl>
+                        <OutlinedInput className="searchButton"
+                        placeholder ="search"
+                        id="input-with-icon-adornment"
+                        value={search}
+                        onChange={SearchText}
+                        style={{ borderRadius: "2rem" }}
+                        startAdornment={
+                            <InputAdornment position="start">
+                            <IconButton size="small" onClick={handleSearch}>
+                                <SearchOutlinedIcon />
+                            </IconButton>
+                            </InputAdornment>
+                        }
+                        endAdornment={
+                            <InputAdornment position="end" onClick={handleCancel}>
+                            <IconButton size="small">
+                                <ClearOutlinedIcon />
+                            </IconButton>
+                            </InputAdornment>
+                        }
+                        />
+                    </FormControl>
+                    </Col>
+                </Row>
+            </Container>
             <Container fluid>
-                <h1>Materials</h1>
                 {loading && <LinearProgress />}
                 <Row>
                     {post.map(data)}
