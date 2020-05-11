8ddd852cc0888ec4161bfc47ed56862b6dc1678f
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-11 15:36:12 +0530

    redisigned 404 page

--- frontend/src/components/messages/errors/404.jsx
+++ frontend/src/components/messages/errors/404.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Jumbotron, Button } from "react-bootstrap";
+import React, { Fragment } from 'react';
+import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
+import Button from '@material-ui/core/Button';
 
-function Emsg() {
-    return <Jumbotron fluid>
-        <h1>404 Not found <span role="img" aria-label="emoji">🤔</span></h1>
-        <p>oops something went wrong <span role="img" aria-label="emoji">😔</span></p>
-        <p>
-            <Button onClick={() => {
-                window.open("/","_self");
-            }} variant="primary">Go To Home</Button>
-        </p>
-    </Jumbotron>
+const error5xx = {
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center",
+    height: "100vh",
+    background: "#fff"
 }
 
-export default Emsg;
+function Error404() {
+    return (
+        <Fragment>
+            <div style={error5xx}>
+                <div>
+                    <span style={{ fontSize: "5rem" }}>4</span><LanguageOutlinedIcon style={{ fontSize: "4.2rem", color: "#ff1744", verticalAlign: "text-bottom" }} /><span style={{ fontSize: "5rem" }}>4</span>
+                    <h1>PAGE DOES NOT EXIST</h1>
+                    <p style={{ display: "block" }}>The page you're looking for does not exist.</p>
+                    <Button onClick={() => {
+                        window.open("/", "_self");
+                    }}>GO HOME</Button>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+export default Error404;
