5816d07f42e49e68d9df7d0cc349f3dcd491d56d
“Santhosh” <santhoshvelr@gmail.com>
2020-05-15 21:11:32 +0530

    port number changed

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,6 @@ app.use('/auth', auth);
 app.use('/materials', materials);
 app.use('/mail', mail);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server started at port 5000");
 });
