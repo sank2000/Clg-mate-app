e8dea2d6e091050ee992ba03617d72fec6a472ca
“Santhosh” <santhoshvelr@gmail.com>
2020-05-16 16:17:23 +0530

    added consolge log messages

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const materials = require('./routes/materials');
 const auth = require('./routes/Auth');
 const mail = require('./routes/mail');
 
+console.log("all require done");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,11 +28,22 @@ mongoose.connect(process.env.DB_URL, {
   useFindAndModify: false,
   useCreateIndex: true
 });
+console.log("before use");
 
 app.use('/posts', posts);
+console.log("after use of post");
+
+
 app.use('/auth', auth);
+console.log("after use of auth");
+
+
 app.use('/materials', materials);
+console.log("after use of material");
+
+
 app.use('/mail', mail);
+console.log("after use of mail");
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started at port 5000");
@@ -39,6 +52,7 @@ app.listen(process.env.PORT || 5000, () => {
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
+  console.log("inside production");
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
