b0d98e21eeb2d2bfa4a99b3ac387528709b8e747
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-16 12:32:33 +0530

    trying a fix for h-10

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.listen(process.env.PORT || 5000, () => {
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
