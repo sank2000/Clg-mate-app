1b63d609a3e22b43327b72d08f335ba1794d9840
“Santhosh” <santhoshvelr@gmail.com>
2020-05-22 22:57:58 +0530

    forgot password feature improved

--- routes/mail.js
+++ routes/mail.js
@@ -4,7 +4,7 @@ const router = express.Router();
 require('dotenv');
 
 const User = require('../models/User');
-
+const OTP =require("../models/OTP");
 let transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
@@ -13,29 +13,6 @@ let transporter = nodemailer.createTransport({
 	}
 });
 
-router.post("/forgot", function (req, res) {
-	User.findOne({ unique_id: req.body.id }, function (err, result) {
-		if (!err) {
-			if (result) {
-				res.json(
-					{
-						find: true,
-						mail: result.email
-					}
-				);
-			}
-			else {
-				res.json({
-					find: false
-				}
-				);
-			}
-		}
-		else {
-			console.log(err);
-		}
-	});
-});
 
 router.post("/feedback", function (req, res) {
 	User.findById(req.session.user, "name", function (err, result) {
@@ -75,50 +52,27 @@ router.post("/feedback", function (req, res) {
 	});
 });
 
-router.post("/forgot/sendmail", function (req, res) {
+
+
+router.post("/forgot", function (req, res) {
 	User.findOne({ unique_id: req.body.id }, function (err, result) {
 		if (!err) {
 			if (result) {
-				let mailOptions =
-				{
-					from: '"Collegemate App" collegematewebapp@gmail.com',
-					to: result.email,
-					subject: "Your Collegemate Sign in password",
-					html: `
-					<h3>Your Collegemate sign in password is:</h3> 
-					<h1>${result.password}</h1>
-					<h4>If you don't know why you're getting this email, consider changing your password to avoid your account being misused/ locked.</h4>
-					`
-				}
-
-				transporter.sendMail(mailOptions, function (err, dat) {
-					if (err) {
-						console.log(err);
-						res.json({
-							msg: "Unable to send Email",
-							done: false
-						}
-						);
-					}
-					else {
-						res.json({
-							msg: "Mail sent Successfully",
-							done: true
-						}
-						);
-					}
-				});
-			}
-			else {
 				res.json(
 					{
-						find: false,
-						done: false,
-						msg: "Unable to send Email",
+						find: true,
+						mail: result.email,
+						unique_id : result.unique_id,
+						doc_id   : result._id
 					}
 				);
 			}
-
+			else {
+				res.json({
+					find: false
+				}
+				);
+			}
 		}
 		else {
 			console.log(err);
@@ -126,4 +80,97 @@ router.post("/forgot/sendmail", function (req, res) {
 	});
 });
 
+
+router.post("/forgot/sendmail", function (req, res) 
+{
+	var num = Math.floor(Math.random() * 900000) + 100000;
+	const nOtp = new OTP(
+		{
+			doc_id : req.body.doc_id,
+            OTP    : num
+		}
+	);
+	nOtp.save(function (err) {
+		if (err) {
+		  console.log(err);
+		}
+		else 
+		{
+			let mailOptions =
+			{
+				from: '"Collegemate App" collegematewebapp@gmail.com',
+				to: req.body.mail,
+				subject: "OTP to reset password",
+				html: `
+				<h3>Your OTP to reset  password is:</h3> 
+				<h1>${num}</h1>
+				<h4>If you don't know why you're getting this email, consider changing your password to avoid your account being misused/ locked.</h4>
+				`
+			}
+
+			transporter.sendMail(mailOptions, function (err, dat) {
+				if (err) {
+					console.log(err);
+					res.json({
+						msg: "Unable to send Email",
+						done: false
+					}
+					);
+				}
+				else {
+					res.json({
+						msg: "Mail sent Successfully",
+						done: true
+					}
+					);
+				}
+		});
+	  }
+	});
+});
+
+router.post("/forgot/verify", function (req, res) 
+{
+	OTP.findOne({ doc_id : req.body.doc_id , OTP : req.body.OTP }, function (err, result) 
+	{
+		if (!err) {
+			if (result) 
+			{
+				res.json({
+				  verified : true
+				});
+			}
+			else
+			{
+				{
+					res.json({
+					  verified : false
+					});
+				}
+			}
+		}
+		else{
+			console.log(err);
+		}
+	})
+});
+
+
+router.post("/forgot/reset", function (req, res) 
+{
+	User.findByIdAndUpdate({ _id: req.body.doc_id },{ password: req.body.password },function(err, result) {
+		  if (err) {
+			res.json({
+				changed : false
+			});
+		  } else {
+			res.json({
+				changed : true
+			});
+		  }
+		}
+	  );
+
+});
+ 
 module.exports = router;
