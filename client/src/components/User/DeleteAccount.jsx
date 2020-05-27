05c94c0ff3c201fcf22e9b65d19e3e9142be356d
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-27 09:27:03 +0530

    added delete account confirmation

--- client/src/components/User/DeleteAccount.jsx
+++ client/src/components/User/DeleteAccount.jsx
@@ -6,7 +6,16 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import {deleteAccount} from "../auth/RouteAccess";
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+import { deleteAccount } from "../auth/RouteAccess";
 import Snackbar from "@material-ui/core/Snackbar";
 import Alert from '../messages/alerts/alert';
 import { Spinner } from "react-bootstrap";
@@ -16,6 +25,19 @@ export default function DeleteAccount() {
   const [agree, setAgree] = useState("");
   const [load, setLoad] = useState(false);
   const [open, setOpen] = useState(false);
+  const [cOpen, setCOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+
+  const handleConfirmOpen = () => {
+    setCOpen(true);
+  };
+
+  const handleConfirmClose = () => {
+    setCOpen(false);
+  };
+
   const [msg, setMsg] = useState({
     content: "",
     type: "error"
@@ -33,17 +55,15 @@ export default function DeleteAccount() {
     setLoad(true);
     const res = await deleteAccount();
     setLoad(false);
-    if (res.data.done) 
-    {
-        setMsg({
-          content: res.data.message,
-          type: "success"
-        });
-        setOpen(true);
-        setTimeout(() =>
-        {
-          authApi.setAuth({auth : false});
-        },3000);
+    if (res.data.done) {
+      setMsg({
+        content: res.data.message,
+        type: "success"
+      });
+      setOpen(true);
+      setTimeout(() => {
+        authApi.setAuth({ auth: false });
+      }, 3000);
     }
     else {
       setMsg({
@@ -56,44 +76,68 @@ export default function DeleteAccount() {
 
   return (
     <>
-    <ExpansionPanel>
-      <ExpansionPanelSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-      >
-        <h5>Delete Account</h5>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <h6>
-              Type{" "}
-              <span>
-                <h5 style={{ display: "inline" }}>"I am Agree"</h5>
-              </span>{" "}
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+        >
+          <h5>Delete Account</h5>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <h6>
+                Type{" "}
+                <span>
+                  <h5 style={{ display: "inline" }}>"I agree"</h5>
+                </span>{" "}
               to continue
             </h6>
-          </Grid>
-          <Grid item xs={12}>
-            <OutlinedInput onChange={e => setAgree(e.target.value)} />
-          </Grid>
-          {agree === "I am Agree" && (
-            <Grid item xs={12}>
-              <Button
-                variant="outlined"
-                color="secondary"
-                style={{ float: "right" }}
-                onClick={() => submit()}
-              >
-                Delete&nbsp;
-              {load && <Spinner animation="border" size="sm" />}
-              </Button>
             </Grid>
-          )}
-        </Grid>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
-    <Snackbar open={open} autoHideDuration={6000} onClose={AhandleClose}>
+            <Grid item xs={12}>
+              <OutlinedInput onChange={e => setAgree(e.target.value)} />
+            </Grid>
+            {agree === "I agree" && (
+              <Grid item xs={12}>
+                <div>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    style={{ float: "right" }}
+                    onClick={handleConfirmOpen}
+                  > <DeleteForeverOutlinedIcon />
+                  Delete&nbsp;
+                  {load && <Spinner animation="border" size="sm" />}
+                  </Button>
+                  <Dialog
+                    open={cOpen}
+                    onClose={handleConfirmClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                    fullScreen={fullScreen}
+                  >
+                    <DialogTitle id="alert-dialog-title">{"Do you really want to delete your account?"}</DialogTitle>
+                    <DialogContent>
+                      <DialogContentText id="alert-dialog-description">
+                        This will remove your account and credentials, including verified email address, DP etc. Any posts or materials you created will not be removed. You can always create a new account using the same ID number and email ID.
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={handleConfirmClose} color="primary">
+                        Cancel
+                      </Button>
+                      <Button onClick={() => submit()} color="secondary" autoFocus>
+                        Yes
+                      </Button>
+                    </DialogActions>
+                  </Dialog>
+                </div>
+              </Grid>
+            )}
+          </Grid>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <Snackbar open={open} autoHideDuration={6000} onClose={AhandleClose}>
         <Alert onClose={AhandleClose} severity={msg.type}>
           {msg.content}
         </Alert>
