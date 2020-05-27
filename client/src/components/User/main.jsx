0aa713cb50716b74e6103f736ed2d18d58b7d6c4
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-27 11:04:47 +0530

    some fixes to profile view

--- client/src/components/User/main.jsx
+++ client/src/components/User/main.jsx
@@ -2,31 +2,34 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import ChangePassword from "./ChangePassword";
 import Card from "./card";
-import DeleteAccouut from "./DeleteAccount";
+import DeleteAccount from "./DeleteAccount";
 import NavigationBar from "../navigation/AppBar";
+import { Container } from "@material-ui/core";
 
 export default function Main() {
-  
+
   return (
     <>
       <NavigationBar title="Profile" />
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        spacing={3}
-      >
-        <Grid item xs={12} sm={4}>
-          <Card />
+      <Container fluid>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item md={10} lg={8}>
+            <Card />
+          </Grid>
+          <Grid item md={6} lg={4}>
+            <ChangePassword />
+          </Grid>
+          <Grid item md={6} lg={4}>
+            <DeleteAccount />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <ChangePassword />
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <DeleteAccouut />
-        </Grid>
-      </Grid>
+      </Container>
     </>
   );
 }
