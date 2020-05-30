7f1c71881641171456589cdac4f94caf6b2dea9b
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-30 15:22:35 +0530

    fix errs

--- client/src/components/main/TimeTable.jsx
+++ client/src/components/main/TimeTable.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
-import Container from '@babel/core/Container';
+import Container from '@material-ui/core/Container';
 import IconButton from "@material-ui/core/IconButton";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,7 +10,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+
 import { time, table } from "../../constants/Table";
 import Schedule from "./Schedule";
 
