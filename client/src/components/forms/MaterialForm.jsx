9725d26a59dc7e9e3f3cd9cd4b0a308a70c56c2a
“Santhosh” <santhoshvelr@gmail.com>
2020-05-30 16:13:03 +0530

    material card changed [1]

--- client/src/components/forms/MaterialForm.jsx
+++ client/src/components/forms/MaterialForm.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Modal } from "react-bootstrap";
 import IconButton from '@material-ui/core/IconButton';
 import TextField from "@material-ui/core/TextField";
 import Btn from "@material-ui/core/Button";
 import CloudUploadOutlinedIcon from "@material-ui/icons/CloudUploadOutlined";
-import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
@@ -13,6 +11,13 @@ import Grid from '@material-ui/core/Grid';
 import LibraryAddOutlinedIcon from "@material-ui/icons/LibraryAddOutlined";
 import Tooltip from '@material-ui/core/Tooltip';
 import Fab from '@material-ui/core/Fab';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
 
 import LinearProgressWithLabel from './LinearProgressWithLabel';
 import subjects from '../../constants/subjects'
@@ -24,6 +29,46 @@ function renderSubjects(subject) {
     </MenuItem>
   );
 }
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: '#ff1a1a',
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
 
 function PostForm(props) {
   const [show, setShow] = useState(false);
@@ -48,6 +93,131 @@ function PostForm(props) {
   return (
     <>
       <Tooltip title="New Material" placement="left">
+        <Fab elevation={3} style={{ position: "fixed", bottom: "23vh", right: "3vw" }} aria-label="add" onClick={handleShow}>
+          <LibraryAddOutlinedIcon style={{ color: '#2196f3' }} />
+        </Fab>
+      </Tooltip>
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={show}>
+        <DialogTitle disableTypography id="customized-dialog-title" onClose={handleClose}>
+          <Typography component="span" variant="h5"> New Material </Typography>
+        </DialogTitle>
+        <form method="post" action="/materials/new">
+          <DialogContent dividers>
+            <TextField style={applyMargin}
+              variant="outlined"
+              required type="text"
+              name="title"
+              fullWidth
+              label="Title" />
+            <Grid container spacing={1}
+              direction="row"
+              justify="space-between"
+              alignItems="center">
+              <Grid item xs={12} sm={6}>
+                <FormControl required variant="outlined" style={applyMargin} size="small" fullWidth >
+                  <InputLabel>
+                    Subject
+                  </InputLabel>
+                  <Select
+                    name="subName"
+                    required
+                    value={subject}
+                    onChange={handleSubjectChange}
+                    label="Subject"
+                  >
+                    <MenuItem disabled={true} value=''>Select a Subject</MenuItem>
+                    {subjects.map(renderSubjects)}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl required variant="outlined" style={applyMargin} size="small" fullWidth >
+                  <InputLabel>
+                    Post Type
+                  </InputLabel>
+                  <Select
+                    name="materialType"
+                    required
+                    value={type}
+                    onChange={handleMaterialTypeChange}
+                    label="Post Type"
+                  >
+                    <MenuItem disabled={true} value="">Select a type</MenuItem>
+                    <MenuItem value={"Book"}>Book</MenuItem>
+                    <MenuItem value={"Form"}>Form</MenuItem>
+                    <MenuItem value={"Question Bank"}>Question Bank</MenuItem>
+                    <MenuItem value={"Question Paper"}>Question Paper</MenuItem>
+                    <MenuItem value={"Notes"}>Notes</MenuItem>
+                    <MenuItem value={"Other"}>Other</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <TextField style={applyMargin}
+              variant="outlined"
+              type="text"
+              name="author"
+              fullWidth
+              label="Author" />
+            <TextField style={applyMargin}
+              variant="outlined"
+              required
+              name="description"
+              rows={4}
+              fullWidth
+              multiline
+              label="Description" />
+
+          </DialogContent>
+          <DialogActions>
+            <Grid container>
+              <div className="file-section" style={applyMargin}>
+                <input id='urls' type="hidden" name="url" value={JSON.stringify(props.url)} />
+                <input
+                  required
+                  style={{ display: "none" }}
+                  type="file"
+                  id="contained-button-file"
+                  name="file"
+                  multiple
+                  onChange={props.handleChange}
+                />
+                <label htmlFor="contained-button-file">
+                  <Btn variant="contained" component="span" disableElevation>
+                    {props.fileChooseState}
+                  </Btn>
+                </label>
+                {props.fileChooseState === 'File Chosen' &&
+                  <Btn
+                    onClick={props.handleUpload}
+                    size="medium"
+                    variant="contained"
+                    color="secondary"
+                    style={{ float: "right", marginRight: "-15px" }}>
+                    <CloudUploadOutlinedIcon
+                      fontSize="small"
+                      className="uploadIcon" />
+                &nbsp; Upload
+              </Btn>
+                }
+                {(props.progress !== -1 && props.progress !== 100) ?
+                  <LinearProgressWithLabel value={props.progress} /> :
+                  (props.progress !== -1 && <p>Uploaded</p>)
+                }
+              </div>
+              {
+                100 === props.progress &&
+                <Btn style={{ margin: "7px", width: "100%", height: "3rem", fontSize: "1.3rem" }} type="submit" size="small" variant="contained" color="primary">Submit</Btn>
+              }
+            </Grid>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </>
+  );
+}
+
+{/* <Tooltip title="New Material" placement="left">
         <Fab elevation={3} style={{ position: "fixed", bottom: "23vh", right: "3vw" }} aria-label="add" onClick={handleShow}>
           <LibraryAddOutlinedIcon style={{ color: '#2196f3' }} />
         </Fab>
@@ -167,9 +337,6 @@ function PostForm(props) {
             </form>
           </div>
         </Modal.Body>
-      </Modal >
-    </>
-  );
-}
+      </Modal > */}
 
 export default PostForm;
