3d79d5bd7bf661a5c514c4dae168101232e1b61f
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-30 12:47:30 +0530

    added new profile banner

---
+++ client/src/components/User/Banner.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+
+import Edit from "./EditDetails";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2)
+  },
+  image: {
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+      height: "100%"
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: 300,
+      height: 300
+    },
+    [theme.breakpoints.up("md")]: {
+      width: 250,
+      height: 250
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: 300,
+      height: 300
+    }
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
+  }
+}));
+
+export default function UserBanner(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper square className={classes.paper}>
+        <Grid container alignItems='center' spacing={2}
+          justify="space-between">
+          <Grid item>
+            <div className={classes.image}>
+              <Avatar
+                alt={props.name}
+                variant="rounded"
+                src={props.avatar}
+                style={Object.assign({}, { width: '100%', height: '100%' }, {
+                  backgroundColor: "orange",
+                  fontSize: "8rem"
+                })}
+              >
+                {props.name[0]}
+              </Avatar>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="h4">
+                  {props.name}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {props.email} <br /> {props.classWithSection}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: {props.id}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Edit />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
--- client/src/components/User/main.jsx
+++ client/src/components/User/main.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import ChangePassword from "./ChangePassword";
-import Card from "./card";
+import BannerRenderer from "./BannerRenderer";
 import DeleteAccount from "./DeleteAccount";
 import NavigationBar from "../navigation/AppBar";
+import { Container } from "@material-ui/core";
 
 export default function Main() {
 
   return (
     <>
       <NavigationBar title="Profile" />
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        spacing={3}
-      >
-        <Grid item md={10} lg={8}>
-          <Card />
+      <Container>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <BannerRenderer />
+          </Grid>
+          <Grid item xs={12} md={4} sm={6} >
+            <ChangePassword />
+          </Grid>
+          <Grid item xs={12} md={4} sm={6}>
+            <DeleteAccount />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4} sm={6} >
-          <ChangePassword />
-        </Grid>
-        <Grid item xs={12} md={4} sm={6}>
-          <DeleteAccount />
-        </Grid>
-      </Grid>
+      </Container>
     </>
   );
 }
