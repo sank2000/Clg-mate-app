e37e58152a4053a095c9f1c9f72ab83bd149bc78
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-05-31 16:25:06 +0530

    improved dark mode toggle

--- client/src/components/dark/Darkmode.jsx
+++ client/src/components/dark/Darkmode.jsx
@@ -5,22 +5,16 @@ import NightsStayIcon from "@material-ui/icons/NightsStay";
 
 import { useTheme, makeStyles } from '@material-ui/styles'
 
-let mode = false;
-if (window.localStorage.getItem("dark")) {
-  if (window.localStorage.getItem("dark") === 'true') {
-    mode = true;
-  }
-}
-
+const mode = window.localStorage.getItem('dark') === 'true' ? true : false;
 
 export default function () {
   const [dark, setDark] = useState(mode);
   const theme = useTheme();
   const classes = makeStyles({
     icon: {
-      fontSize: '1.4rem',
+      fontSize: '1.42rem',
       background: dark ? theme.palette.primary.dark : theme.palette.primary.main,
-      padding: '3px',
+      padding: '2px',
       color: dark ? '#ffeb3b' : '#ffa000',
       borderRadius: '100%'
     }
